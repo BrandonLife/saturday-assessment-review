d18c429941b4f5b2fb1c59ee6a219a7bc4b7b54e
Brandon Life <blife1119@gmail.com>
2023-03-18 13:02:19 -0400

    finished review assessment

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// Obtain the img element, and assign it to the movie variable 
-let movie
+// Obtain the img element, and assign it to the movie variable
+let movie = document.getElementById("movie");
 
 // Obtain the 'change-border' button, and assign it to the changeBorder variable
-let changeBorder
+let changeBorder = document.getElementById("change-border");
 
 //DO NOT CHANGE THE FOLLOWING CODE
-changeBorder.addEventListener("click", function() {
+changeBorder.addEventListener("click", function () {
   if (movie.style.borderRadius == "0px") {
-    movie.style.borderRadius = "25px"
+    movie.style.borderRadius = "25px";
+  } else {
+    movie.style.borderRadius = "0px";
   }
-  else {
-    movie.style.borderRadius = "0px"
-  }
-})
+});
